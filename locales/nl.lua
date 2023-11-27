c9874afbde9e763f088f38c2bf201b9d01350a10
Matti <57048210+MattiVboiii@users.noreply.github.com>
2023-11-27 22:35:07 +0100

    Create nl.lua

---
+++ locales/nl.lua
@@ -0,0 +1,29 @@
+Loc["nl"] = {
+    notify = {
+        ["load_link"] = "Link laden: ",
+        ["new_volume"] = "Volume: ",
+    },
+    target = {
+        ["dj_booth"] = "DJ Booth",
+    },
+    menu = {
+        ["no_song"] = "🔇 Geen nummer wordt afgespeeld",
+        ["cur_playing"] = "Momenteel aan het spelen: ",
+        ["cur_paused"] = "Momenteel gepauzeerd: ",
+        ["live"] = "🔴 Live",
+        ["close"] = "Sluit",
+        ["play"] = "Een nummer afspelen",
+        ["history"] = "Geschiedenis",
+        ["text_pause"] = "Muziek pauzeren",
+        ["text_resume"] = "Hervat Muziek",
+        ["volume"] = "Volume: ",
+        ["stop"] = "Stop muziek",
+        ["back"] = "Terug",
+        ["history_play"] = "Geschiedenis - Druk om af te spelen",
+        ["youtube_url"] = "YouTube URL",
+        ["range"] = "Min: 0 - Max: 100",
+        ["submit"] = "Bevestig",
+        ["select"] = "Selectie van liedjes",
+        ["music_volume"] = "Muziekvolume",
+    },
+}
